df74afa554616a7546bbb405f911bad87878c618
Guimill <guillaume.milleret@gmail.com>
2023-04-07 15:33:16 +0200

    error root config

--- template/src/routes/root.jsx
+++ template/src/routes/root.jsx
@@ -1,16 +1,23 @@
 import Banner from "../components/Banner"
 import Footer from "../components/footer"
 import Navbar from "../components/navbar"
-import { Oultet } from "react-router-dom"
+import { useRouteError } from "react-router-dom"
 
 export default function Index() {
+
+    const error = useRouterError();
+    console.error(error);
 
     return (
         <>
         <Banner />
         <Navbar />
         <main>
-            <Oultet />
+            <div>
+                <h1>Oops!</h1>
+                <p>Une petite erreur de parcours</p>
+                <b> { error.statusText || error.message } </b>
+            </div>
         </main>
         <Footer />
         </>
