813b42ddf68cf5a4d68ab47a52319cf8085e9157
Guimill <guillaume.milleret@gmail.com>
2023-04-07 16:05:44 +0200

    correction

--- template/src/routes/error.jsx
+++ template/src/routes/error.jsx
@@ -1,7 +1,17 @@
+import { useRouteError } from "react-router-dom"
+
 export default function Index() {
+
+    const error = useRouterError();
+    console.error(error);
 
     return (
         <>
+            <div>
+                <h1>Oops!</h1>
+                <p>Une petite erreur de parcours</p>
+                <b> {error.statusText || error.message} </b>
+            </div>
         </>
     )
 
--- template/src/routes/root.jsx
+++ template/src/routes/root.jsx
@@ -1,25 +1,18 @@
 import Banner from "../components/Banner"
 import Footer from "../components/footer"
 import Navbar from "../components/navbar"
-import { useRouteError } from "react-router-dom"
+import { Outlet } from "react-router-dom"
 
 export default function Index() {
 
-    const error = useRouterError();
-    console.error(error);
-
     return (
         <>
-        <Banner />
-        <Navbar />
-        <main>
-            <div>
-                <h1>Oops!</h1>
-                <p>Une petite erreur de parcours</p>
-                <b> { error.statusText || error.message } </b>
-            </div>
-        </main>
-        <Footer />
+            <Banner />
+            <Navbar />
+            <main>
+                <Outlet />
+            </main>
+            <Footer />
         </>
     )
 
